bf7bf76ddfa5029d78af01c7ebf19212da0ddff6
fs-dlh <faruk.sari@ai.dlh.lu>
2026-04-22 17:49:51 +0200

    Update 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for name in sorted(names):
+    filtered_names = [name for name in names if not name.startswith("__")]
+    for name in sorted(filtered_names):
         print(name)

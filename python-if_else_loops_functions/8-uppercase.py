e98017b8a6fc11f36ca1a911c6fe3d909de6e402
fs-dlh <faruk.sari@ai.dlh.lu>
2026-04-22 14:49:09 +0200

    9-print_last_digit.py

--- python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    result = ""
-    for c in str:
-        if 'a' <= c <= 'z':
-            result += chr(ord(c) - 32)
-        else:
-            result += c
-    print("{}".format(result))

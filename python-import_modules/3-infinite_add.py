9efcf908a61b3c1f9f9975ce05c20fbcc5b06963
fs-dlh <faruk.sari@ai.dlh.lu>
2026-04-22 17:16:37 +0200

    Update 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv[1:]
-total = 0
-for arg in argv:
-    total += int(arg)
-print(total)
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    total = 0
+    for arg in argv:
+        total += int(arg)
+    print(total)

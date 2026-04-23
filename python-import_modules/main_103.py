b57a22198b5f0588d3efae8a44e495e7998dbd12
fs-dlh <faruk.sari@ai.dlh.lu>
2026-04-23 10:27:36 +0200

    updates

--- python-import_modules/main_103.py
+++ python-import_modules/main_103.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from 102-magic_calculation import magic_calculation
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, magic_calculation(a, b))) 
+import dis
+from magic_calculation_102 import magic_calculation
+dis.dis(magic_calculation)

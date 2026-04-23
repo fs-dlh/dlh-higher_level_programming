f272d393f1659d9cb8a67fce7b8c5741b719aed3
fs-dlh <faruk.sari@ai.dlh.lu>
2026-04-23 09:59:27 +0200

    Update 102-magic_calculation.py

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -2,5 +2,11 @@
 import dis
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
-    return result
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
 dis.dis(magic_calculation)

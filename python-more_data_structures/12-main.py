54fba99d1c56b16b3764ba990ddded80feafe537
fs-dlh <faruk.sari@ai.dlh.lu>
2026-04-28 15:49:14 +0200

    Files added for task 12

--- python-more_data_structures/12-main.py
+++ python-more_data_structures/12-main.py
@@ -18,5 +18,3 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "MCMLXIX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))

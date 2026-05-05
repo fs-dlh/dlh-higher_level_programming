b71f8f2ef70aacdca027c20059a637774ac6cf7b
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-05 20:36:49 +0200

    square.py files

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module containing a Square class with private size attribute."""
+"""This module defines a Square class with private size attribute."""
+
+
 class Square:
+    """This class represents an Square class with private size attribute."""
     def __init__(self, size):
         self.__size = size

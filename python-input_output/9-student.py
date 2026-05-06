d00249fedc305afff57711695f53e55dfcb39f95
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-06 21:57:21 +0200

    Task 9 commit

---
+++ python-input_output/9-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module that defines a Student class.
+"""
+
+
+class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of the Student instance.
+
+        Returns:
+            dict: A dictionary containing  first_name, last_name, and age.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }

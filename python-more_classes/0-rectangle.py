1d423d13eea1c71ff04bf32c05dc90bbc4a4b037
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-06 10:20:17 +0200

    python-more_clases first commit

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines an empty Rectangle class for future implementation."""
+
+
+class Rectangle:
+    """This represents an empty Rectangle class with no attributes/methods."""
+    pass

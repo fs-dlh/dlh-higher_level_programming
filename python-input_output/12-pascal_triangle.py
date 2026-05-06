4e5616fa39e7aef1662ec7776662aed157b3598c
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-06 23:18:10 +0200

    Task 12 commit

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+"""
+Module that defines a Pascal's Triangle class.
+"""
+
 
 triangle = [[], [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
 
 
 def pascal_triangle(n):
+    """
+    Initializes a new Student instance.
+
+    Args:
+        n (int): Number of rows to generate.
+
+    Returns:
+        list: A list of lists representing the Pascal's Triangle.
+    """
+
     if n <= 0:
         return triangle[:1]
     return triangle[:n+1]

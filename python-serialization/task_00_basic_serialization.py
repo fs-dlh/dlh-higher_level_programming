3a4c0f4c65e950f03fb54cf545b00b9a66fd3b1f
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-07 16:51:11 +0200

    task 0 commit

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This module provides functions for serializing data to JSON,
+as well as loading and deserializing data from a JSON file.
+"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serialize the given data to JSON and save it to the specified file."""
+
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """
+    Load and deserialize data from the specified JSON file.
+    """
+
+    with open(filename, 'r') as f:
+        return json.load(f)

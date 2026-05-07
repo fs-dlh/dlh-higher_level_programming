0522ab8cbfa0f75136de0460f84d1f979076c79c
fs-dlh <faruk.sari@ai.dlh.lu>
2026-05-07 15:34:19 +0200

    Task 12 commit

--- python-input_output/test.py
+++ python-input_output/test.py
@@ -11,8 +11,12 @@ def print_triangle(triangle):
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
-    print ("Lenght: {}".format(len(triangle)))
+    print ("\nLenght: {}\n\n".format(len(triangle)))
 
+    new_triangle = triangle[::-1]
+                            
+    for row in new_triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))   
 
 if __name__ == "__main__":
-    print_triangle(pascal_triangle(100))
+    print_triangle(pascal_triangle(8))
